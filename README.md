6a9c5d11bef5ff88ad81fe96a55bbabc88615042
Danny Lau <dnnlau@gmail.com>
2022-10-17 09:27:49 -0700

    Add CSS, add images, update README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Tic-Tac-Toe
+# Rock Paper Scissor
+
+Welcome to my version of Rock, Paper and Scissors! :fist: :hand: :v:
+
+This game was created using HTML, CSS, and Javascript.
+
+You get to compete against a computer for glory! 
+
+Choose among rock:rock:, paper:page_facing_up:, or scissors:scissors:!
+
+The computer will also make a random selection.
+
+The first to 5 points wins :boom:
+
+I had a lot fun making this game and I hope you enjoy it!
+
+
+
--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,10 @@
+const outcome = document.querySelector('.result')
+
+// Used to display respective scores on page
+const playerScoreDisplay = document.querySelector('.playerscorenumber')
+const computerScoreDisplay = document.querySelector('.computerscorenumber')
+
+
 let playerScore = 0
 let computerScore = 0
 
@@ -7,6 +14,7 @@ buttons.forEach((button) => {
     button.addEventListener('click', getPlayerChoice)
 })
 
+// Passing the player's choice to game()
 function getPlayerChoice(button) {
     console.log(this.id)
     playerChoice = this.id
@@ -15,17 +23,22 @@ function getPlayerChoice(button) {
 
 // This function takes the user's input and plays playRound 5 times. First to 5 points wins
 function game(playerChoice) {
-    // Initialize scores for player and computer
+    // Getting the reult of 1 match by passing player's choice and computer choice
     result = playRound(playerChoice, getComputerChoice())
+    playerScoreDisplay.textContent = `${playerScore}`
+    computerScoreDisplay.textContent = `${computerScore}`
 
     if (result == 1) {
         playerScore += 1
+        playerScoreDisplay.textContent = `${playerScore}`
     }
 
     else if (result == -1) {
         computerScore += 1
+        computerScoreDisplay.textContent = `${computerScore}`
     }
-    console.log("Player Score: " + playerScore + " Computer Score: " + computerScore)
+
+    // Check to see if there is a winner after each round
     winnerCheck()
 }
 
@@ -44,29 +57,35 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissor" ||
         playerSelection === "Paper" && computerSelection === "Rock" ||
         playerSelection === "Scissor" && computerSelection === "Paper") {
-        console.log("You Win! You selected " + playerSelection + " and that beats " + computerSelection + "!")
+        outcome.textContent = `You win! You selected ${playerSelection} and that beats ${computerSelection}`
         return 1
     }
 
+
     // Conditional case that results in a tie
     else if (playerSelection == computerSelection) {
-        console.log("It's a Tie! You both selected " + playerSelection + "!")
+        outcome.textContent = `It's a tie! You both selected ${playerSelection}!`
         return 0
     }
 
     // All other options which the computer wins 
     else {
-        console.log("You lose! The computer selected " + computerSelection + " and that beats " + playerSelection + "!")
+        outcome.textContent = `You lose! My professor selected ${computerSelection} and that beats ${playerSelection}`
         return -1
     }
 
 }
+// Checking the winner. If one person wins, it resets the score after the player hits a button
 function winnerCheck() {
     if (playerScore == 5) {
-        console.log("You are the first to 5 points! You Win!")
+        outcome.textContent = `You are the first to 5 points! You win!`
+        playerScore = 0
+        computerScore = 0
     }
     else if (computerScore == 5) {
-        console.log("The computer is the first to 5 points. You Lose :(")
+        outcome.textContent = `The professor is the first to 5 points. You lose!`
+        playerScore = 0
+        computerScore = 0
     }
 }
 
